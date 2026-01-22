d9df1fd949f6b36b0f669792b768e8e634111e39
5arimNadeem <sarimnadeem141@gmail.com>
2026-01-22 19:14:57 +0500

    url-shortner

---
+++ url-shortner/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+
+const { connectToMongoDB } = require("./connect")
+const URL = require('./models/url')
+const urlRoute = require('./routes/url')
+
+const app = express()
+
+const port = 8001
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+    .then(() => console.log("Connected to mongoDB"))
+
+app.use(express.json())
+
+app.use('/url', urlRoute)
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, {
+        $push: {
+            visitHistory: { timestamp: Date.now() },
+        },
+    })
+    res.redirect(entry.redirectURL)
+})
+
+app.listen(port, () => console.log("Server started at port 8001"))
